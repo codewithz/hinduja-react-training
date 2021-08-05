cbfa9dc500f7ce9823e49f93e8dd43a1c46e37e6
Zartab Nakhwa <nzartab@gmail.com>
2021-08-05 19:17:03 +0530

    Rendering List

--- src/components/cart/CartHome.jsx
+++ src/components/cart/CartHome.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React,{useState} from 'react'
 import ProductComponent from './ProductComponent'
 
 export default function CartHome() {
+
+    const data=['A','B','C','D','E'];
+
+    const [tags,setTags]=useState(data);
+
     return (
         <div>
             <ProductComponent />
+          
+            <ul>
+                {
+                    tags.map(element =>
+                    <li>{element}</li>
+                        )
+                }
+            </ul>
+            
             
         </div>
     )
